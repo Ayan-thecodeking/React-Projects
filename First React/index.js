bf60759d7050f4fa8c9e18db8196798e1181b7c4
Ayan-thecodeking <guptajiayan111@gmail.com>
2022-11-23 17:27:22 +0530

    added

---
+++ First React/index.js
@@ -0,0 +1,10 @@
+
+// ReactDOM.render(<p>hey ayan here</p> , document.GetElementById("root"))
+
+ReactDOM.render(
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>,
+    document.getElementById("root")
+)
